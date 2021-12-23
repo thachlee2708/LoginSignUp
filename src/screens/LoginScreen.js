016761df71dfaef64afcfe10126530d61885d8ea
Lê Ngọc Thạch <thachpro2708@gmail.com>
2021-12-23 10:39:12 +0700

    commitupdate

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -14,29 +14,60 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 Icon.loadFont();
 export default LoginScreen = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(null);
+  const [isValidPassword, setIsValidPassword] = useState(null);
+  const [password, setPassword] = useState('');
+  const checkValidEmail = email => {
+    email.match(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/)
+      ? setIsValidEmail(true)
+      : setIsValidEmail(false);
+  };
+  const checkValidPassword = password => {
+    password.length > 0 ? setIsValidPassword(true) : setIsValidPassword(false);
+  };
   const [isPassHide, setIsPassHide] = useState(true);
   const [icon, setIcon] = useState('eye');
   const onPressHidePass = () => {
     isPassHide ? setIsPassHide(false) : setIsPassHide(true);
     isPassHide ? setIcon('eye-slash') : setIcon('eye');
   };
+  const checkLogin = () => {
+    checkValidEmail(email);
+    checkValidPassword(password);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.login}>Login</Text>
         <Text style={styles.emailPassword}>Email Address</Text>
-        <TextInput style={styles.emailInput}></TextInput>
+        <TextInput
+          style={isValidEmail ? styles.emailInput : styles.emailInputInvalid}
+          onChangeText={setEmail}></TextInput>
+        {isValidEmail === false ? (
+          <Text style={styles.helperText}>
+            {email.length === 0 ? 'Email required' : 'Email invalid'}
+          </Text>
+        ) : null}
         <Text style={styles.emailPassword}>Password</Text>
-        <View style={styles.wrapPasswordInput}>
+        <View
+          style={
+            isValidPassword
+              ? styles.wrapPasswordInput
+              : styles.wrapPasswordInputInvalid
+          }>
           <TextInput
+            onChangeText={setPassword}
             style={styles.passwordInput}
             secureTextEntry={isPassHide}></TextInput>
-
           <TouchableOpacity onPress={onPressHidePass}>
             <Icon name={icon} size={20} color="#05A4C5" />
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.buttonLogin}>
+        {isValidPassword === false ? (
+          <Text style={styles.helperText}>Password required</Text>
+        ) : null}
+        <TouchableOpacity style={styles.buttonLogin} onPress={checkLogin}>
           <Text style={styles.buttonLoginColorText}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity>
@@ -69,17 +100,29 @@ const styles = StyleSheet.create({
   },
   emailInput: {
     paddingVertical: 5,
-    fontSize: 20,
+    fontSize: 15,
     borderBottomWidth: 0.8,
+    borderColor: '#000',
+  },
+  emailInputInvalid: {
+    paddingVertical: 5,
+    fontSize: 18,
+    borderBottomWidth: 0.8,
+    borderColor: '#F11111',
   },
   wrapPasswordInput: {
     borderBottomWidth: 0.8,
     flexDirection: 'row',
   },
+  wrapPasswordInputInvalid: {
+    borderBottomWidth: 0.8,
+    flexDirection: 'row',
+    borderColor: '#F11111',
+  },
   passwordInput: {
     paddingVertical: 5,
     flex: 1,
-    fontSize: 20,
+    fontSize: 15,
   },
   buttonLogin: {
     margin: 20,
@@ -104,5 +147,9 @@ const styles = StyleSheet.create({
   registerText: {
     color: '#05A4C5',
     fontSize: 15,
+  },
+  helperText: {
+    paddingVertical: 10,
+    color: '#F11111',
   },
 });
